75bcb365f3e7c91e17c11d91bad790b906151e36
buckethead1986 <rspeckpfs@gmail.com>
2017-09-21 18:22:26 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor :roster, :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+    # @roster[grade] = []
+    # @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    output = {}
+      @roster.each do |grade, student_array|
+        output[grade] = student_array.sort_by {|name| name.downcase}
+      end
+    output
+  end
+end
